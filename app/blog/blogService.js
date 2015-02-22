7ff69961ca75bb22ad1f411abeed494d80088a3f
Ethan Williams <ethan.r.williams@live.com>
2015-02-22 11:11:10 +1100

    gh-pages angular post

--- app/blog/blogService.js
+++ app/blog/blogService.js
@@ -7,8 +7,8 @@ blogServices.factory('blogService', [function(){
   
   function _getAll(){
     return [
-      {title: 'first.', partial: 'first-post'},
-      //{title: "Github Pages & Angular's Html5Mode", partial: 'gh-pages-angular'}
+      {title: 'First.', partial: 'first-post'},
+      {title: "GitHub Pages & Angular's Html5Mode", partial: 'gh-pages-angular'}
     ];
   }
   
